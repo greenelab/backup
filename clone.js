0b2fb88d84721c5d7d0bec6a687129466cc4e28a
Vincent Rubinetti <vince.rubinetti@gmail.com>
2021-05-06 15:12:37 -0400

    add some comments

--- clone.js
+++ clone.js
@@ -10,14 +10,23 @@ const cloneRepos = async (repos) => {
   log();
   info(`Cloning repos`);
 
+  // get repo urls to clone
   let lines = [];
   for (const { html_url, wiki_url } of repos) {
     lines.push(html_url);
     lines.push(wiki_url);
   }
+
+  // remove empty lines, and prepend the "git clone" command
   lines = lines.filter((line) => line).map((line) => `git clone ${line}`);
+
+  // make folder to contain clones and nav to it
   lines = [`mkdir -p ${dir}`, `cd ${dir}`].concat(lines);
+
+  // write bash script out to file
   fs.writeFile("clone.sh", lines.join("\n"), () => null);
+
+  // bash script is then run by "yarn clone" script
 };
 
 // clone script
