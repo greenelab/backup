e3acf49da6bf8158d4528d474a240ffbd7707d6f
Vincent Rubinetti <vince.rubinetti@gmail.com>
2021-05-06 14:39:33 -0400

    update

--- clone.js
+++ clone.js
@@ -2,12 +2,15 @@ const fs = require("fs");
 const { getRepos } = require("./github");
 const { log, info } = require("./util");
 
+// clone directory
+const dir = "clone" 
+
 // clone org's repos locally
 const cloneRepos = async (repos) => {
   log();
   info(`Cloning repos`);
 
-  let lines = ["mkdir -p clone", "cd clone"];
+  let lines = [`mkdir -p ${dir}`, `cd ${dir}`];
   for (const { html_url, wiki_url } of repos) {
     lines.push(html_url);
     lines.push(wiki_url);
