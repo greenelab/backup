d2bd67eadd55dc02b5e619cd628d616ab3736cca
Vincent Rubinetti <vince.rubinetti@gmail.com>
2021-05-12 14:33:22 -0400

    optimize wiki backup

--- github.js
+++ github.js
@@ -1,6 +1,8 @@
 const { Octokit } = require("@octokit/rest");
 const { githubToken, githubOrganization } = require("./vars");
-const { log, success, info } = require("./util");
+const { log, success, info, error } = require("./util");
+const simpleGit = require("simple-git");
+const git = simpleGit();
 
 // GitHub API helper
 const octokit = new Octokit({ auth: githubToken });
@@ -20,23 +22,46 @@ const getRepos = async () => {
     const options = { org: "org", per_page: "100", page };
     const results = (await octokit.request(url, options)).data;
 
-    // get wiki url for repo if it has one
-    results.forEach((repo) => {
-      if (repo.has_wiki) repo.wiki_url = repo.html_url + ".wiki.git";
-    });
-
     // stop if reached end page,
     if (!results.length) break;
     else repos = repos.concat(results);
   }
 
-  // count public and private
+  // count public, private, wikis
   const public = repos.filter((repo) => !repo.private).length;
   const private = repos.filter((repo) => repo.private).length;
-
   success(`Found ${repos.length} repos, ${public} public, ${private} private`);
 
+  log();
+  info("Looking for wikis");
+
+  // check each repo for a wiki
+  for (const repo of repos) {
+    const wiki_url = repo.html_url + ".wiki.git";
+    if (repo.has_wiki && (await checkRepo(wiki_url))) repo.wiki_url = wiki_url;
+  }
+
+  // count wikis
+  const wikis = repos.filter((repo) => repo.wiki_url).length;
+  success(`Found ${wikis} wikis`);
+
+  // count total
+  success(`${repos.length + wikis} total repos to archive`);
+
   return repos;
+};
+
+// check if (wiki) repo exists
+// https://stackoverflow.com/questions/23914896/check-that-git-repository-exists
+const checkRepo = async (repo) => {
+  try {
+    await git.listRemote([repo]);
+    // success(repo);
+    return true;
+  } catch (message) {
+    // error(repo);
+    return false;
+  }
 };
 
 module.exports = { getRepos };
